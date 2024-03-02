7007e240b0c9ed1104665c99e68993520f9c124b
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2024-03-02 16:41:10 +0100

    Ajout des commentaires dans ppm.h

--- ppm.h
+++ ppm.h
@@ -1,12 +1,14 @@
 #ifndef PPM_H
 #define PPM_H
 
+// Structure représentant un pixel dans une image PPM
 struct ppm_pixel {
   unsigned char r;
   unsigned char g;
   unsigned char b;
 };
 
+// Fonction permettant de définir les valeurs RVB d'un pixel
 static inline void ppm_setpixel(struct ppm_pixel *px, unsigned char r,
                                 unsigned char g, unsigned char b) {
   px->r = r;
@@ -14,15 +16,20 @@ static inline void ppm_setpixel(struct ppm_pixel *px, unsigned char r,
   px->b = b;
 }
 
+// Structure de représentation d'une image PPM
 struct ppm_image {
   unsigned int width;
   unsigned int height;
   struct ppm_pixel *px;
 };
 
+// Fonction permettant d'initialiser une image PPM avec la largeur et la hauteur données et réservant la mémoire nécessaire
 int ppm_image_init(struct ppm_image *im, int w, int h);
+
+// Fonction permettant de libérer la mémoire allouée à une image PPM
 int ppm_image_release(struct ppm_image *im);
 
+// Fonction permettant de définir les valeurs RVB d'un pixel dans une image PPM
 static inline void ppm_image_setpixel(struct ppm_image *im, int x, int y,
                                       unsigned char r, unsigned char g,
                                       unsigned char b) {
@@ -30,6 +37,7 @@ static inline void ppm_image_setpixel(struct ppm_image *im, int x, int y,
   ppm_setpixel(px, r, g, b);
 }
 
+// Fonction pour sauvegarder une image PPM dans un fichier
 int ppm_image_dump(struct ppm_image *im, char *path);
 
-#endif /* PPM_H */
+#endif
